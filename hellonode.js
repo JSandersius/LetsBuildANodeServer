d2869550d603923adea73f2aa8b2c5282c969a24
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-29 13:25:28 -0700

    installed new version of express

--- hellonode.js
+++ hellonode.js
@@ -1,5 +1,15 @@
 // this only uses Node, not Express
 
+
+
+//This code imports the "http" module and uses it to create a server 
+//(createServer()) that listens for HTTP requests on port 8000. 
+//The script then prints a message to the console about what browser 
+//URL you can use to test the server. The createServer() function 
+//takes as an argument a callback function that will be invoked 
+//when an HTTP request is received — this simply returns a response
+//with an HTTP status code of 200 ("OK") and the plain text "Hello World".
+
 //Load HTTP module
 var http = require("http");
 
